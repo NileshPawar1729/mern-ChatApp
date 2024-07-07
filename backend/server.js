6af0c40d38ef3dfe48db82ce78c32c89246a5214
NileshPawar1729 <141761096+NileshPawar1729@users.noreply.github.com>
2024-07-07 17:24:21 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -9,6 +10,8 @@ import { app, server } from './socket/socket.js';
 
 const PORT = process.env.Port || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());//to parse the incoming request with json payloads(from req.body)
@@ -18,11 +21,11 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-// app.get("/",(req,res)=>{
-//      res.send('Hello world!!!');
-//  })
-
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(5000,()=>{
     connectToMongoDB();
